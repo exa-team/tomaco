17d39e3b479a6e1cf2bdcd250313a65d35c311a0
Cleiton <cleiton.mittmann@zoop.com.br>
2019-06-27 21:01:06 -0300

    feat: Add pause button
    Case the focus mode is active, the user can pause the timer without stop (reseting) it just by
    clicking the pause button. When user the wants to return to the focus mode, just to click the
    resume button.
    
    closes #16

--- tomaco/static/src/js/main.js
+++ tomaco/static/src/js/main.js
@@ -5,20 +5,27 @@ const FOCUS_STARTING_FROM = 25 * 60;
 const BREAK_STARTING_FROM = 5 * 60;
 const START_TEXT = "Start!";
 const STOP_TEXT = "Stop!";
+const PAUSE_TEXT = "Pause!";
+const RESUME_TEXT = "Resume!";
 
 export default class Timer {
-  constructor($display, $button, $finishedCounter) {
+  constructor($display, $button, $finishedCounter, $pauseButton) {
     this.$display = $display;
     this.$button = $button;
     this.$finishedCounter = $finishedCounter;
-
+    this.$pauseButton = $pauseButton;
     this.seconds = FOCUS_STARTING_FROM;
     this.focusMode = true;
     this.isRunning = false;
+    this.isPaused = false;
     this.timer = null;
     this.finishedPomodoros = 0;
 
     this.$button.addEventListener("click", this.toggleTimer.bind(this));
+    this.$pauseButton.addEventListener(
+      "click",
+      this.togglePauseTimer.bind(this)
+    );
   }
 
   updateUI() {
@@ -30,7 +37,19 @@ export default class Timer {
       this.$button.classList.remove("red");
     }
 
+    // updating main button
     this.$button.innerHTML = this.isRunning ? STOP_TEXT : START_TEXT;
+
+    // updating break time button
+    if (this.isRunning) {
+      this.$pauseButton.disabled = false;
+      this.$pauseButton.innerText = this.isPaused ? RESUME_TEXT : PAUSE_TEXT;
+    } else {
+      this.$pauseButton.disabled = true;
+      this.$pauseButton.innerText = PAUSE_TEXT;
+    }
+
+    // updatime time
     this.$display.innerHTML = secondsToMinutesAndSeconds(this.seconds);
     this.$finishedCounter.innerHTML = Array.from({
       length: this.finishedPomodoros
@@ -50,6 +69,23 @@ export default class Timer {
     clearInterval(this.timer);
     this.seconds = this.focusMode ? FOCUS_STARTING_FROM : BREAK_STARTING_FROM;
     this.isRunning = false;
+    this.isPaused = false;
+  }
+
+  togglePauseTimer() {
+    if (!this.isRunning) {
+      return; // only toggle if pommodoro is running
+    }
+
+    // recreate setInterval avoids unnecessary processing
+    if (this.isPaused) {
+      this.startTimer();
+    } else {
+      clearInterval(this.timer);
+    }
+
+    this.isPaused = !this.isPaused;
+    this.updateUI();
   }
 
   toggleTimer() {
@@ -99,8 +135,8 @@ export default class Timer {
     this.updateUI();
   }
 
-  static build($display, $button, $finishedCounter) {
-    const timer = new Timer($display, $button, $finishedCounter);
+  static build($display, $button, $finishedCounter, $pauseButton) {
+    const timer = new Timer($display, $button, $finishedCounter, $pauseButton);
     timer.stopTimer();
     timer.updateUI();
 
