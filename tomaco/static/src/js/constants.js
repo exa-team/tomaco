e3be807101ead931d569fcee657550c752a21695
Klaus Laube <klaus.laube@tradeshift.com>
2019-07-04 15:46:35 +0200

    fix: Focus and break message in notification
    We were showing the same "Pomodoro done" message when rest and focus mode were done. With this
    change, we differ the messages given which timer has ended.
    
    fix #37

---
+++ tomaco/static/src/js/constants.js
@@ -0,0 +1,12 @@
+export const BREAK_STARTING_FROM = 5 * 60;
+export const FOCUS_STARTING_FROM = 25 * 60;
+export const PAUSE_TEXT = "Pause!";
+export const RESUME_TEXT = "Resume!";
+export const ONE_SECOND = 1 * 1000;
+export const START_TEXT = "Start!";
+export const STOP_TEXT = "Stop!";
+
+export const MESSAGES = {
+  FOCUS_MODE_DONE: "Pomodoro done! Take a break.",
+  REST_MODE_DONE: "Get back to work! :D"
+};
