ccc3a2ec299255272e8ef38cea0bf1375fadb1fa
Klaus Laube <klaus.laube@tradeshift.com>
2019-07-16 12:03:41 +0200

    chore: Configure database access
    This is an enabler for next tickets. We are deciding to use Postgres as database and Alembic
    (Flask-Migrate) as a migration engine.
    
    connects to #21

--- tomaco/__init__.py
+++ tomaco/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from . import views
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("settings.py")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
     views.init_app(app)
 
     return app
