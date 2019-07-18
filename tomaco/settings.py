8b4413a87817770df2921c33442e77ffb75263a6
Klaus Laube <klaus.laube@tradeshift.com>
2019-07-18 15:14:35 +0200

    chore: Configure database access (#55)
    This is an enabler for next tickets. We are deciding to use Postgres as database and Alembic
    (Flask-Migrate) as a migration engine.
    
    connects to #21

--- tomaco/settings.py
+++ tomaco/settings.py
@@ -2,13 +2,24 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "should-be-a-secret-key")
 
+# Database
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URL", "postgresql://localhost/tomaco"
+)
+
 # Auth
 GITHUB_CLIENT_ID = os.environ.get("GITHUB_CLIENT_ID", "should-be-client-id")
 GITHUB_CLIENT_SECRET = os.environ.get("GITHUB_CLIENT_SECRET", "should-be-client-secret")
 GITHUB_CALLBACK_URI = os.environ.get(
     "GITHUB_CALLBACK_URI", "http://localhost:8080/login/complete"
 )
-GITHUB_AUTHORIZE_URL = "https://github.com/login/oauth/authorize"
-GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token"
-GITHUB_USER_RESOURCE_URL = "https://api.github.com/user"
+GITHUB_AUTHORIZE_URL = os.environ.get(
+    "GITHUB_AUTHORIZE_URL", "https://github.com/login/oauth/authorize"
+)
+GITHUB_ACCESS_TOKEN_URL = os.environ.get(
+    "GITHUB_ACCESS_TOKEN_URL", "https://github.com/login/oauth/access_token"
+)
+GITHUB_USER_RESOURCE_URL = os.environ.get(
+    "GITHUB_USER_RESOURCE_URL", "https://api.github.com/user"
+)
 GITHUB_SCOPE = "email"
